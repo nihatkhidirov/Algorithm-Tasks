dc25ad8865393fc170bd00297053f779a6cf186e
hp <hp@DESKTOP-77U60MC>
2023-02-05 03:30:32 +0400

    Add project files.

---
+++ Algorithm Tasks/Program.cs
@@ -0,0 +1,77 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Algorithm_Tasks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Task 2
+            //Console.Write("Ededinizi Daxil Edin: ");
+            //int number = int.Parse(Console.ReadLine());
+            //int sum = 0; int count = 0;
+
+            //count = number.ToString().Length;
+
+            //for (int a = count; a > 0; a--)
+            //{
+            //    int digit = number % 10;
+            //    sum = sum + (a * digit);
+            //    number /= 10;
+            //}
+            //Console.WriteLine("Reqemlerin cemi : " + sum);
+            #endregion
+
+
+
+            {
+                #region
+                //Console.Write("Ededinizi Daxil edin : ");
+                //int number = int.Parse(Console.ReadLine());
+                //int sum = 0; int position = 1;
+
+                //while (number > 0)
+                //{
+                //    int digitofnumbers = number % 10;
+                //    if (position % 2 == 0)
+                //    {
+                //        sum = sum + digitofnumbers * digitofnumbers;
+                //    }
+                //    position++;
+                //    number /= 10;
+                //}
+
+                //Console.WriteLine("Cut xanadaki ededlerin cemi: " + sum);
+
+                #endregion
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
